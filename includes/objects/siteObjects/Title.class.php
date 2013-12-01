eec408afcb82bfe322834d9b814b457bac0ea85f
Farzin Fadamin <ffadamin@gmail.com>
2013-12-01 20:15:09 +0330

    change page titles

--- includes/objects/siteObjects/Title.class.php
+++ includes/objects/siteObjects/Title.class.php
@@ -10,10 +10,22 @@
 		public function process(){
 			
 
-			$title = "CSS3Designer";
+			$title = "DCT MotorSports";
 			switch(LiteFrame::getActiveAction()){
 				case "homepage":
 					$title = "DCT MotorSports | Homepage";
+					break;
+				case "products":
+					$title = "DCT MotorSports | Products";
+					break;
+				case "about":
+					$title = "DCT MotorSports | About";
+					break;
+				case "contact":
+					$title = "DCT MotorSports | Contact Us";
+					break;
+				case "faq":
+					$title = "DCT MotorSports | Frequently Asked Questions";
 					break;
 			}
 			$this->results = $title;
